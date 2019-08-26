66ae2aaa9857c461763fcbbc31b5f2e6b9d11905
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-26 17:35:43 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -2,10 +2,19 @@ import sys
 from st2common.runners.base_action import Action
 
 class MyAction(Action):
-
-    def run(self, a,b,c):
-        print("Sum="+str(a+b+c))
-        print("Type of a= "+str(type(a)))
-        print("Type of b= "+str(type(b)))
-        print("Type of c= "+str(type(c)))
-        return(True,a+b+c)
+    n1 = 0
+    n2 = 1
+    count = 0
+    def run(self, a):
+            if a<=1 :
+            print("Enter a number greater than 1")
+            else :
+                print("Fibonacci sequence upto",nterms,":")
+                    while count < nterms:
+                        print(n1,end=' , ')
+                        nth = n1 + n2
+                        # update values
+                        n1 = n2
+                        n2 = nth
+                        count += 1
+         return(True)
