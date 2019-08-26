7ac147f1597e5294cc35125d8754370e80fcee92
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-26 17:47:22 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -5,16 +5,16 @@ class MyAction(Action):
     n1 = 0
     n2 = 1
     count = 0
-        def run(self, a):
-                if a<=1 :
-                  print("Enter a number greater than 1")
-                else :
-                    print("Fibonacci sequence upto",nterms,":")
-                    while count < nterms:
-                        print(n1,end=' , ')
-                        nth = n1 + n2
-                        # update values
-                        n1 = n2
-                        n2 = nth
-                        count += 1
-            return(True)
+    def run(self, a):
+        if a<=1 :
+            print("Enter a number greater than 1")
+        else :
+            print("Fibonacci sequence upto",nterms,":")
+            while count < nterms:
+                print(n1,end=' , ')
+                nth = n1 + n2
+                # update values
+                n1 = n2
+                n2 = nth
+                count += 1
+        return(True)
