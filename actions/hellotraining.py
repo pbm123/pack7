ecac8b436170cdd864ceaa9b140dc1ccc771c752
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-26 17:06:52 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -3,6 +3,9 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
-    def run(self, message1,message2,message3):
-        print("Sum="+str(message1+message2+message3))
-        return(True,message1+message2+message3)
+    def run(self, a,b,c):
+        print("Sum="+str(a+b+c))
+        print("Type of a= "+type(a))
+        print("Type of b= "+type(b))
+        print("Type of c= "+type(c))
+        return(True,a+b+c)
