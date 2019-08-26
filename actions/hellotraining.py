ec9a4fe0e55a52315adb25cb9183d3b4edc75f9f
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-26 17:11:05 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -5,7 +5,7 @@ class MyAction(Action):
 
     def run(self, a,b,c):
         print("Sum="+str(a+b+c))
-        print("Type of a= "+type(a))
-        print("Type of b= "+type(b))
-        print("Type of c= "+type(c))
+        print("Type of a= "+str(type(a)))
+        print("Type of b= "+str(type(b)))
+        print("Type of c= "+str(type(c)))
         return(True,a+b+c)
